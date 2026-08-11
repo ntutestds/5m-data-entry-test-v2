5cd5f6820c644d9a80d6f358f4af385776a75461
ntutestds <foamstand@gmail.com>
2026-08-11 21:14:31 +0800

    Fix count_evens function to count even numbers
    Implemented the count_evens function to count even numbers in a list.

--- src/q8.py
+++ src/q8.py
@@ -20,19 +20,26 @@ Task
 
 (a) What does the buggy version actually return for [1, 2, 3, 4, 5, 6, 8], and why?
 
-    Answer:
+    Answer: 3 
+    In the above function, it is looking for numbers with a remainder of 1 when divided by 2. 
+    Hence, it will print the number of odd numbers in the list.
 
 (b) Fix the bug. Write the corrected function below.
     (A one-character change is enough, but you must understand why.)
 """
 
 def count_evens(numbers):
-    # your corrected code here
-    pass
+    count = 0
+    for n in numbers:
+        if n % 2 == 0:
+            count = count + 1
+    return count
+print(count_evens([1, 2, 3, 4, 5, 6, 8]))
 
 
 """
 (c) In one sentence, explain in plain English what `n % 2 == 0` checks.
 
-    Answer:
+    Answer: When a number is divided by 2, it checks whether the remainder is 0. 
+    Hence, it checks whether a number is an even number.
 """
